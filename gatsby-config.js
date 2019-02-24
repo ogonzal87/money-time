817b574dbbb41b8ccd4aa8902c1335545938eac0
Oscar G <ogonzal87@gmail.com>
2019-02-24 15:24:16 -0800

    finished

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Money Time`,
+    description: `Find out how much a meeting costs...`,
+    author: `@ogonzal87`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -17,16 +17,17 @@ module.exports = {
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+      // options: {
+      //   name: `gatsby-starter-default`,
+      //   short_name: `starter`,
+      //   start_url: `/`,
+      //   background_color: `#663399`,
+      //   theme_color: `#663399`,
+      //   display: `minimal-ui`,
+      //   icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      // },
     },
+    `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import "./layout.scss"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -17,24 +15,28 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
+      <div>
+        <main style={{
+          display: `flex`,
+          alignItems: `center`,
+          justifyContent: `center`,
+          height: `100vh`
+        }}>
+
+          {children}
+
+        </main>
+
+        <footer style={{
+          position: `fixed`,
+          bottom: `16px`,
+          left: `16px`,
+          opacity: `.4`
+        }} className="ds-caption-text-style">
+          Built by <a href="https://oskrhq.com/">Oscar Gonzalez</a>
+        </footer>
+
+      </div>
     )}
   />
 )
