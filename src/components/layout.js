392a3b15b7273b440fc07736e3fbc2b7518e6123
Oscar G <ogonzal87@gmail.com>
2019-02-28 19:35:46 -0800

    fixing height error

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,7 +20,6 @@ const Layout = ({ children }) => (
           display: `flex`,
           alignItems: `center`,
           justifyContent: `center`,
-          height: `100vh`
         }}>
 
           {children}
